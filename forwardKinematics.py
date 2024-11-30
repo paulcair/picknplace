d419e1390359fc551eb1b81ff3975b3fcc89de44
paul-cairns <paulcair@gmail.com>
2024-11-30 08:34:08 -0500

    updating runservo.py

---
+++ forwardKinematics.py
@@ -0,0 +1,35 @@
+"""
+forwardKinematics.py
+
+This is a script to control leArm 6DOF robot via serial connection. This script is written to perform forward kinematics and output the predicted (x,y,z) coordinates of the end effector.
+This script is used to control servos via a serial connection. It includes functions to:
+- Take in desired servo angles.
+- Defines the D-H Transformation matrices and performs matrix multiplications to detirmine the x,y,z coordinates.
+- Moves the servos and the end effector to the desired positions by calling runServo.py.
+
+The script initializes a serial connection, defines action arrays, and iterates over servos to perform movements.
+
+Author: Paul Cairns
+Date: Nov 30th, 2024
+"""
+import math
+import numpy as np
+import runServo
+
+#input desired joint angles (0-180) in degrees and desired claw width (0<x<30)
+theta_1_deg = 90
+theta_2_deg = 90
+theta_3_deg = 90
+theta_4_deg = 90
+theta_5_deg = 90
+tool_x = 15
+
+# Convert the inputs into radians for matrix multiplications 
+theta_1 = math.radians(theta_1_deg)
+theta_2 = math.radians(theta_2_deg)
+theta_3 = math.radians(theta_3_deg)
+theta_4 = math.radians(theta_4_deg)
+theta_5 = math.radians(theta_5_deg)
+
+# Convert the inputs into servo values by maping them from degrees to a value between 500 and 2500 that will be sent to the servo. Note that servos are defined in reverse order as the thetas.
+S_6 =  (30-tool_x)*2000/60+1500
