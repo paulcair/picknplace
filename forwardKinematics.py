73936864b5a88aadf7d59b7c170a03d955cbbb70
paul-cairns <paulcair@gmail.com>
2024-11-30 09:17:12 -0500

    updating forwardKinematics.py

--- forwardKinematics.py
+++ forwardKinematics.py
@@ -32,6 +32,11 @@ theta_4 = math.radians(theta_4_deg)
 theta_5 = math.radians(theta_5_deg)
 
 # Convert the inputs into servo values by maping them from degrees to a value between 500 and 2500 that will be sent to the servo. Note that servos are defined in reverse order as the thetas.
-S_6 =  (30-tool_x)*2000/60+1500
+S_1 = int((30-tool_x)*2000/60+1500)
+S_2 = int(theta_5_deg/180*2000+500)
+S_3 = int(theta_4_deg/180*2000+500)
+S_4 = int(theta_3_deg/180*2000+500)
+S_5 = int(theta_2_deg/180*2000+500)
+S_6 = int(theta_1_deg/180*2000+500)    
 
-print(S_6)
+
